37694ec8d7a9630090c68ade8136b2c336f3868d
jsalgua027 <jsalcedoguarde@gmail.com>
2022-12-01 10:12:26 +0100

    otra proyecto de prueba

---
+++ Prueba2/src/main/java/com/mycompany/prueba2/Prueba2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.prueba2;
+
+/**
+ *
+ * @author nacho
+ */
+public class Prueba2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        //otras preubas
+    }
+}
