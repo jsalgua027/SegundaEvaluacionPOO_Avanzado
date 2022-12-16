2bb359d7e29ae7bb1e8be90c1d01f56dad486167
jsalgua027 <jsalcedoguarde@gmail.com>
2022-12-16 10:53:25 +0100

    mas usos con las listas

--- Tema5ejercicios/src/main/java/listas/ListaNumeros.java
+++ Tema5ejercicios/src/main/java/listas/ListaNumeros.java
@@ -17,16 +17,53 @@ public class ListaNumeros {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        ArrayList<Integer> listaEnteros= new ArrayList<>();
-        
+
+        ArrayList<Integer> listaEnteros = new ArrayList<>();
+
         listaEnteros.add(10);
         listaEnteros.add(9);
         listaEnteros.add(8);
         listaEnteros.add(7);
         listaEnteros.add(6);
-     
-        
+        System.out.println("El tamaño de lista: " + listaEnteros.size());
+
+        for (int i = 0; i < listaEnteros.size(); i++) {
+            System.out.println("Elemento " + (i + 1) + " - >" + listaEnteros.get(i));
+            System.out.println("Posicion " + (i) + "->" + listaEnteros.get(i));
+
+        }
+
+        if (listaEnteros.contains(7)) {
+            System.out.println("El 7 esta en la lista");
+
+        }
+        // en que posicion esta el si
+        int posicion = listaEnteros.indexOf(6);
+        System.out.println("Posicion del 6 esta en:  " + posicion);
+
+        // borrar OJO QUE EL METODO NO DISITINGE ENTRE UN INT DE POSICION O EL INTEGER OBJETO
+        // borro el objeto
+        Integer numero = 10;
+
+        listaEnteros.remove(numero);
+
+        // muestro 
+        listaEnteros.forEach(System.out::println);
+
+        System.out.println("-------------------------");
+
+        // otra forma 
+        for (Integer num : listaEnteros) {
+            System.out.println(num);
+        }
+
+        // para cambiar el valor dando la posicion y el valor 
+        listaEnteros.set(0, 200);
+
+        for (Integer num : listaEnteros) {
+            System.out.println(num);
+        }
+
     }
-    
+
 }
