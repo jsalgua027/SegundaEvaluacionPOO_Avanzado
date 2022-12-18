b3797c498912ce9cf2ebe233aa15ce6a281ecb25
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2022-12-18 13:06:44 +0100

    no hace correctamente la copia 3

--- Tema5ejercicios/src/main/java/tema5ejercicios/Ejer023.java
+++ Tema5ejercicios/src/main/java/tema5ejercicios/Ejer023.java
@@ -5,6 +5,7 @@
 package tema5ejercicios;
 
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Random;
 
 /**
@@ -16,10 +17,9 @@ public class Ejer023 {
     /**
      * @param args the command line arguments
      */
-    
-      private static Random aleatorio= new Random();
-    
-        public static int aleatorioDentroRango(int min, int max) {
+    private static Random aleatorio = new Random();
+
+    public static int aleatorioDentroRango(int min, int max) {
         int numeroGenerado;
 
         numeroGenerado = aleatorio.nextInt(max - min + 1) + min;
@@ -27,60 +27,82 @@ public class Ejer023 {
         return numeroGenerado;
 
     }
+
     public static void main(String[] args) {
-      
-        int tamanio1=aleatorioDentroRango(5, 15);
-        int tamanio2=aleatorioDentroRango(10, 20);
-        int valoresArray=0;
+
+        int tamanio1 = aleatorioDentroRango(5, 15);
+        int tamanio2 = aleatorioDentroRango(10, 20);
+        int valoresArray = 0;
         System.out.println(tamanio1);
-           ArrayList<Integer> lista1 = new ArrayList<>(tamanio1);
-           ArrayList<Integer> lista2 = new ArrayList<>(tamanio2);
-           ArrayList<Integer> lista3 = new ArrayList<>();
-           
-           // agrego valores aleatorios a lista 1
-           for (int i = 0; i < tamanio1; i++) {
-               valoresArray=aleatorioDentroRango(50, 100);
+        ArrayList<Integer> lista1 = new ArrayList<>(tamanio1);
+        ArrayList<Integer> lista2 = new ArrayList<>(tamanio2);
+        ArrayList<Integer> lista3 = new ArrayList<>();
+        ArrayList<Integer> lista4 = new ArrayList<>();
+
+        // agrego valores aleatorios a lista 1
+        for (int i = 0; i < tamanio1; i++) {
+            valoresArray = aleatorioDentroRango(50, 100);
             lista1.add(valoresArray);
         }
-      
-        
-            // agrego valores aleatorios a lista 2
-           for (int i = 0; i < tamanio2; i++) {
-               valoresArray=aleatorioDentroRango(50, 100);
+
+        // agrego valores aleatorios a lista 2
+        for (int i = 0; i < tamanio2; i++) {
+            valoresArray = aleatorioDentroRango(50, 100);
             lista2.add(valoresArray);
         }
-           
-           System.out.println("*****IMPRIMO LAS DOS LISTAS***************");
-           
-           lista1.forEach(System.out::println);
-           System.out.println("***************************************************");
-             lista2.forEach(System.out::println);
-             
-             //primero busco cual de la dos listas es mayor para usarlo para el bucle
-            if (lista1.size()>lista2.size()){
-             for (int i = 0; i < tamanio1 ;i++) {
-                    if(lista1.get(i).compareTo(lista2.get(i))!=0){
-                     lista3.add(lista1.get(i));
-                     lista3.add(lista2.get(i));
-                    }
-                }
-            }else if (lista2.size()>lista1.size()){
-                for (int i = 0; i < tamanio2; i++) {
-                    
-                     if(lista2.get(i).compareTo(lista1.get(i))!=0){
-                     lista3.add(lista2.get(i));
-                     lista3.add(lista1.get(i));
-                    }
-                }
-            
-            }
-            
-             System.out.println("Imprimo la lista 3");
-                lista3.forEach(System.out::println);
-             
-            
-        }
-           
-    }
-    
 
+        System.out.println("*****IMPRIMO LAS DOS LISTAS***************");
+        System.out.println("Imprimo la lista 1");
+        lista1.forEach(System.out::println);
+        System.out.println("***************************************************");
+        System.out.println("Imprimo la lista 2");
+        lista2.forEach(System.out::println);
+
+        System.out.println("*************************************************");
+        for (int i = 0; i < lista1.size(); i++) {
+            lista3.add(lista1.get(i));
+
+        }
+
+        for (int i = 0; i < lista2.size(); i++) {
+            for (int j = 0; j < lista3.size(); j++) {
+                if (lista3.get(i).equals(lista2.get(i)) == false) {
+                    lista3.add(lista2.get(i));
+
+                }
+            }
+
+        }
+
+        System.out.println(
+                "Imprimo la lista 3 con los valores de lista 1 y lista 2 sin que se repitan");
+        lista3.forEach(System.out::println);
+
+        System.out.println(
+                "********************************************************");
+
+        for (int i = 0;
+                i < lista1.size();
+                i++) {
+            if (lista1.get(i) % 2 == 0) {// elementos pares de lista 1
+
+                lista4.add(lista1.get(i));
+            }
+        }
+
+        for (int i = 0;
+                i < lista2.size();
+                i++) {
+            if (!(lista2.get(i) % 2 == 0)) {// elementos pares de lista 1
+
+                lista4.add(lista2.get(i));
+            }
+        }
+
+        System.out.println(
+                "Imprimo la lista 4 con valores pares(lista1) y valores impares (lista2)");
+        lista4.forEach(System.out::println);
+
+    }
+
+}
