2b23d1bbcb3e6c5b3033751820945d6752a91dce
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2022-12-19 18:47:46 +0100

    borrado el paquete lanzar la moneda, añadido paquete ejercicio24 lista de canciones, pendiente los dos metodos de busqueda

---
+++ Tema5ejercicios/src/main/java/ejercicio24_cancion/Cancion.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio24_cancion;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Windows10
+ */
+public class Cancion {
+    
+    private String titulo;
+    private String cantante;
+
+    private double duracion;// minutos 
+
+    public Cancion() {
+    }
+
+    public Cancion(String nombre, String estilo, double duracion) {
+        this.titulo = nombre;
+        this.cantante = estilo;
+     
+        this.duracion = duracion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCantante() {
+        return cantante;
+    }
+
+    public void setCantante(String cantante) {
+        this.cantante = cantante;
+    }
+
+    
+
+  
+
+    public double getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(double duracion) {
+        this.duracion = duracion;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cancion{");
+        sb.append("titulo=").append(titulo);
+        sb.append(", cantante=").append(cantante);
+        sb.append(", duracion=").append(duracion);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 83 * hash + Objects.hashCode(this.titulo);
+        hash = 83 * hash + Objects.hashCode(this.cantante);
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.duracion) ^ (Double.doubleToLongBits(this.duracion) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cancion other = (Cancion) obj;
+        if (!Objects.equals(this.titulo, other.titulo)) {
+            return false;
+        }
+        return Objects.equals(this.cantante, other.cantante);
+    }
+
+    
+
+   
+    
+    
+    
+    
+    
+}
---
+++ Tema5ejercicios/src/main/java/ejercicio24_cancion/ListaReproduccion.java
@@ -0,0 +1,133 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio24_cancion;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author Windows10
+ */
+public class ListaReproduccion {
+    
+    private List<Cancion> canciones;
+    
+    public ListaReproduccion(int tamanio) {
+        
+        tamanio = Math.abs(tamanio);
+        
+        this.canciones = new ArrayList<>(tamanio);
+        
+        for (int i = 0; i < tamanio; i++) {
+            this.canciones.add(new Cancion());
+        }
+        
+    }
+
+    // numero de canciones
+    public int numeroCanciones() {
+        
+        return this.canciones.size();
+    }
+
+    // si esta vacio
+    public boolean estaVacia() {
+        boolean siLoEsta;// true es vacia
+
+        if (numeroCanciones() == 0) {
+            siLoEsta = true;
+        } else {
+            siLoEsta = false;
+        }
+        
+        return siLoEsta;
+        
+    }
+
+    // ecuchar cancion segun posición
+    public Cancion escucharCancion(int posicion) {
+        
+        return canciones.get(posicion);
+    }
+
+    // cambiar canción de la posicion inidicada por otra propocionada
+    public void cambiarCancion(int posicion, Cancion c) {
+        
+        this.canciones.set(posicion, c);
+        
+    }
+
+    // añadir cancion al final de la lista
+    public void añadirCancion(Cancion c) {
+        
+        this.canciones.add(c);
+    }
+
+    //borrar cancion posición indicada
+    public boolean borraCancionPosicion(int posicion) {
+        
+        if (posicion >= 0) {
+            this.canciones.remove(this.canciones.get(posicion));
+            return true;
+        }
+        return false;
+    }
+
+    // borrar pasando la cancion
+    public boolean borraCancion(Cancion c) {
+        
+        if (this.canciones.contains(c)) {
+            this.canciones.remove(c);
+            return true;
+        }
+        return false;
+        
+    }
+
+    // imprimir lista
+    public String imprimirLista() {
+        
+        String tmp = "";
+        
+        for (Cancion v : this.canciones) {
+            
+            tmp += v.toString() + "\n";
+            
+        }
+        
+        return tmp;
+    }
+
+    // buscar cancion
+    public int buscarCancion(Cancion c) {
+        
+        return this.canciones.indexOf(c);
+    }
+
+    //ordenar por titulo
+    public void ordenarListaTitulo() {
+        
+        Collections.sort(this.canciones, (c1, c2) -> c1.getTitulo().compareToIgnoreCase(c2.getTitulo()));
+        
+    }
+
+    //ordenar por cantante
+    public void ordenarListaCantante() {
+        
+        Collections.sort(this.canciones, (c1, c2) -> c1.getCantante().compareToIgnoreCase(c2.getCantante()));
+    }
+    
+    //busqueda por titulo
+//   public int  buscarPorTitulo(String titulo){
+//       Cancion aux = new Cancion();
+//       aux.setTitulo(titulo);
+//       Collections.sort(this.canciones, (c1, c2) -> c1.getTitulo().compareToIgnoreCase(c2.getTitulo()));
+//   int posicion=Collections.binarySearch( this.canciones, buscarCancion(aux));
+//   return posicion;
+//   }
+//    
+}
