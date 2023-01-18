a7b36001f4e553e96c558145481af29f786ae594
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-01-19 00:26:32 +0100

    funciona, falta refacorizar el codigo en metodos como indica el ejercicio

--- Ej5ENachoSalcedoMatrices/src/main/java/apartado_b_arbolito/Arbolito.java
+++ Ej5ENachoSalcedoMatrices/src/main/java/apartado_b_arbolito/Arbolito.java
@@ -15,7 +15,6 @@ public class Arbolito {
     /**
      * @param args the command line arguments
      */
-    
 //        *                       fila 0 espacios 7
 //      ***                      fila 1 espacios 6
 //     *****                    fila 2 espacios 5
@@ -26,70 +25,93 @@ public class Arbolito {
 //***************          fila 8 espacios 0
 //          *                     fila 9 espacios 7
 //          *                     fila 10 espacios 7
-    
-     public static void imprimirMatriz(char[][] origen) {
+    public static void imprimirMatriz(char[][] origen) {
 
         for (int i = 0; i < origen.length; i++) {
             for (int j = 0; j < origen[i].length; j++) {
-                System.out.print( origen[i][j] );
+                System.out.print(origen[i][j]);
             }
             System.out.println("");
         }
     }
-     
-     public static char[][] rellenarArbol( int filas, int coumnas ){
-       int altura= filas-2;
-       int contadorPosicionesLibres=0;
-       int contadorPosicionesOcupadas=0;
-      char  libre=' ';
-      char relleno='1';
-     char[][] arbol = new char [filas][coumnas];
-         for (int i = 0; i < arbol.length; i++) {
-             for (int j = 0; j < arbol[i].length; j++) {
-               if(j<altura-j || j>altura+j){
-               arbol[i][j]=libre;
-               }else{
-               arbol[i][j]=relleno;
-               }
-             }
-         }
-     
-     
-     return arbol;
-     }
-    
+
+    public static char[][] rellenarArbol(int filas, int coumnas) {
+        int altura = filas - 2;
+        int contadorPosicionesLibres = 0;
+        int contadorPosicionesOcupadas = 0;
+        char libre = ' ';
+        char relleno = '1';
+        char[][] arbol = new char[filas][coumnas];
+        for (int i = 0; i < arbol.length; i++) {
+            for (int j = 0; j < arbol[i].length; j++) {
+                if (j < altura - j || j > altura + j) {
+                    arbol[i][j] = libre;
+                } else {
+                    arbol[i][j] = relleno;
+                }
+            }
+        }
+
+        return arbol;
+    }
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int altura=0;
-         char  libre='x';
-      char relleno='1';
+        int altura = 0;
+        char libre = ' ';
+        char relleno = '1';
         // obtego el numero de filas 
-        int numeroColumnas= 1;
-        
+        int numeroColumnas = 1;
+
         System.out.println("Dime la altura de la copa del arbol");
-        altura= teclado.nextInt();
-         int numeroFilas =  altura+2;
+        altura = teclado.nextInt();
+        int numeroFilas = altura + 2;
+
         // bucle para obtener el numero de columnas
-        for (int i = 0; i < altura-1; i++) {
-            numeroColumnas= numeroColumnas+2;
+        for (int i = 0; i < altura - 1; i++) {
+            numeroColumnas = numeroColumnas + 2;
         }
-            System.out.println("El numero de columnas seria:  " + numeroColumnas+ " El numero de filas: "+numeroFilas);
-        
+
+        int derecha = altura - 1;
+        int izquierda = altura - 1;
+       
         char[][] arbol = new char[numeroFilas][numeroColumnas];
-         for (int i = 0; i < arbol.length; i++) {
-             for (int j = 0; j < arbol[i].length; j++) {
-               if(!(j<altura-j && j>altura+j)){
-               arbol[i][j]=libre;
-               }else{
-               arbol[i][j]=relleno;
-               }
-             }
-         }
+        //bucle
+        for (int i = 0; i < arbol.length; i++) {
+
+            for (int j = 0; j < arbol[i].length; j++) {
+
+                if (j >= izquierda && j <= derecha) {
+                    arbol[i][j] = relleno;
+
+                } else {
+                    arbol[i][j] = libre;
+                }
+            }
+            izquierda -= 1;
+            derecha += 1;
+
+            if (i >= altura) {
+                derecha = altura - 1;
+                izquierda = altura - 1;
+                for (int j = 0; j < arbol[i].length; j++) {
+
+                    if (j >= izquierda && j <= derecha) {
+                        arbol[i][j] = relleno;
+
+                    } else {
+                        arbol[i][j] = libre;
+                    }
+
+                }
+
+            }
+
+        }
         System.out.println("");
-        
+
         imprimirMatriz(arbol);
-        
-        
+
     }
-    
+
 }
