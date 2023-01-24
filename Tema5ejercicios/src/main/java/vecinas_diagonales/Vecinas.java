604d34efbe9881c951c644e5e9fd90c4fd42f639
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-01-24 18:27:43 +0100

    vecinas iniciado

--- Tema5ejercicios/src/main/java/vecinas_diagonales/Vecinas.java
+++ Tema5ejercicios/src/main/java/vecinas_diagonales/Vecinas.java
@@ -18,8 +18,8 @@ public class Vecinas {
      * @param args the command line arguments
      */
     private static Random aleatorio = new Random();
-     private static Scanner teclado = new Scanner(System.in);
-    
+    private static Scanner teclado = new Scanner(System.in);
+
     public static int aleatorioDentroRango(int min, int max) {
         int numeroGenerado;
 
@@ -38,8 +38,8 @@ public class Vecinas {
 
         return origen;
     }
-    
-      public static int leerEnteroSinErroresScanner() {
+
+    public static int leerEnteroSinErroresScanner() {
         int num = 0;
         boolean repetir = true;
 
@@ -60,7 +60,8 @@ public class Vecinas {
 
         return num;
     }
-      public static void imprimirMatriz(int[][] origen) {
+
+    public static void imprimirMatriz(int[][] origen) {
 
         for (int i = 0; i < origen.length; i++) {
             for (int j = 0; j < origen[i].length; j++) {
@@ -69,41 +70,61 @@ public class Vecinas {
             System.out.println("");
         }
     }
-      
-      public static void vecinas( int[][]orien, int fila, int columna){
-      
-         /*compruebo que las coordenadas estan en una parte de la matriz que tiene huecos libres
-          en todas las direcciones 
-      */
-         
-            if ((orien[fila].length-columna>=1&& columna>=1)&&(fila>1&&orien.length-fila>=1)){
-                        
-      }
-      
-      }
-    
+
+    public static void vecinas(int[][] origen, int coorFila, int coorColum) {
+        int tamanio=origen.length-1;
+        // compruebo que las coordenadas esten dentro de el tamaño de la matriz
+        if ((coorFila > 0 && coorFila < tamanio) && (coorColum > 0 && coorColum < tamanio)) {
+
+           //Si hay espacios en los dos ejes
+            if((coorFila>=1&& tamanio-coorFila>=1)&&(coorColum>=1&& tamanio-coorColum>=1)){
+            
+            
+            }
+            //Esquina superior izquierda
+           if(coorFila==0 && coorColum==0){
+           
+           
+           } 
+           //Esquina inferior izquierda
+           if(coorFila== tamanio && coorColum==0){
+           
+           }
+           //Esquina superior derecha
+           if (coorFila==0&& coorColum==tamanio){
+           
+           
+           }
+           //Esquina inferior derecha
+           if(coorFila==tamanio && coorColum==tamanio){
+           
+           }
+           //colunma cero  y fila mayor que cero
+           
+           
+        }
+
+    }
+
     public static void main(String[] args) {
-        int filas=0;
-        int columnas=0;
-        int coordenadaF=0;
-        int coordenadaC=0;
+        int filas = 0;
+        int columnas = 0;
+        int coordenadaF = 0;
+        int coordenadaC = 0;
         System.out.println(" Indique le numero de filas");
-        filas=leerEnteroSinErroresScanner();
+        filas = leerEnteroSinErroresScanner();
         System.out.println("Indique el numero de columnas");
-        columnas=leerEnteroSinErroresScanner();
-        int [][] matrizVecinas = new int[filas][columnas];
+        columnas = leerEnteroSinErroresScanner();
+        int[][] matrizVecinas = new int[filas][columnas];
         rellenarMatrizAleatorioa(matrizVecinas, 1, 20);
         imprimirMatriz(matrizVecinas);
-        
-        System.out.println("Vamos a ver las vecinas");
-        System.out.println("Coordendas x: fila");
-        coordenadaF=leerEnteroSinErroresScanner();
-        System.out.println("Coordendas y: coolumna");
-        coordenadaC=leerEnteroSinErroresScanner();
-        
-        
-        
-        
+
+//        System.out.println("Vamos a ver las vecinas");
+//        System.out.println("Coordendas x: fila");
+//        coordenadaF=leerEnteroSinErroresScanner();
+//        System.out.println("Coordendas y: coolumna");
+//        coordenadaC=leerEnteroSinErroresScanner();
+//        
     }
-    
+
 }
