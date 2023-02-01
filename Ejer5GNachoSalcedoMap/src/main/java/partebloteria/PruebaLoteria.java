46ff0f1b378495e164afd147638d4dfb0e06f90b
jsalgua027 <jsalcedoguarde@gmail.com>
2023-02-01 14:15:03 +0100

    Añadido contador de voletos

--- Ejer5GNachoSalcedoMap/src/main/java/partebloteria/PruebaLoteria.java
+++ Ejer5GNachoSalcedoMap/src/main/java/partebloteria/PruebaLoteria.java
@@ -15,49 +15,46 @@ import java.util.Scanner;
  */
 public class PruebaLoteria {
 
-  
-  
- 
-    public static Map<NumeroLoteria,Premio> generarMapLoteria(int cantidadNumeros){
-  Map<NumeroLoteria,Premio> aux = new HashMap<>();
-        
+    public static Map<NumeroLoteria, Premio> generarMapLoteria(int cantidadNumeros) {
+        Map<NumeroLoteria, Premio> aux = new HashMap<>();
+
         for (int i = 0; i < cantidadNumeros; i++) {
             aux.put(new NumeroLoteria(), new Premio());
         }
-  
-    return aux;
+
+        return aux;
     }
-    
-    public static Premio tienePremio( Map<NumeroLoteria,Premio> lista,NumeroLoteria aux){
-    
+
+    public static Premio tienePremio(Map<NumeroLoteria, Premio> lista, NumeroLoteria aux) {
+
         return lista.get(aux);
-    
+
     }
-    
-    
+
     public static void main(String[] args) {
-    Scanner teclado = new Scanner(System.in);
-        Premio solicitado ;
-        NumeroLoteria numeroPremiado= new NumeroLoteria();
-      
-  Map<NumeroLoteria,Premio> loteria = generarMapLoteria(100);
-  
-  for (Map.Entry<NumeroLoteria, Premio> entry : loteria.entrySet()) {
-    System.out.println("El boleto es: " + entry.getKey() +" -- " + entry.getValue());
-  
-  }
-  
+        Scanner teclado = new Scanner(System.in);
+        Premio solicitado;
+        NumeroLoteria numeroPremiado = new NumeroLoteria();
+
+        Map<NumeroLoteria, Premio> loteria = generarMapLoteria(1000);
+
+        for (Map.Entry<NumeroLoteria, Premio> entry : loteria.entrySet()) {
+            System.out.println("El boleto es: " + entry.getKey() + " -- " + entry.getValue());
+
+        }
+
+        System.out.println("¿Cuantos numeros hay:  ? " + loteria.size());
+
         System.out.println("Indique el numero que quiere comprobar");
         numeroPremiado.setNumero(teclado.nextLine());
-        solicitado=tienePremio(loteria, numeroPremiado);
-        if (loteria.containsKey(numeroPremiado)){
-        System.out.println("El: " + numeroPremiado.toString()+ " tiene el premio de: " + solicitado.toString());
-        }else{
-        
+        solicitado = tienePremio(loteria, numeroPremiado);
+        if (loteria.containsKey(numeroPremiado)) {
+            System.out.println("El: " + numeroPremiado.toString() + " tiene el premio de: " + solicitado.toString());
+        } else {
+
             System.out.println("El Número indicado no existe");
         }
-        
-  
+
     }
-    
+
 }
