c0a6b7246eeae7668ca7edaf9060d31d7162adc0
jsalgua027 <jsalcedoguarde@gmail.com>
2023-02-02 12:44:32 +0100

    pila casi terminada

--- Tema5ejercicios/src/main/java/ejercicio26_pilagenericayejercicio27/Pila.java
+++ Tema5ejercicios/src/main/java/ejercicio26_pilagenericayejercicio27/Pila.java
@@ -4,6 +4,7 @@
  */
 package ejercicio26_pilagenericayejercicio27;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,9 +13,9 @@ import java.util.List;
  * @author nacho
  */
 public class Pila<T> {
-    
-    private List<T> pila ;
-    
+
+    private List<T> pila;
+
 // constructo generico
     public Pila() {
         this.pila = new ArrayList<>();
@@ -27,7 +28,72 @@ public class Pila<T> {
     public void setPila(List<T> pila) {
         this.pila = pila;
     }
-    
-    
-    
+
+    // metodo push
+    public void push(T aux) {
+        this.pila.add(aux);
+
+    }
+
+    //metedo pop
+    public void pop(T aux) {
+        this.pila.remove(this.pila.size() - 1);
+
+    }
+
+    // esta vacia
+    public boolean estaVacia() {
+        return this.pila.isEmpty();
+
+    }
+
+    // esta llena 
+    public boolean estaLlena() {
+        return this.pila.isEmpty();
+
+    }
+    // cuantos elementos
+
+    public int cuantosElementos() {
+
+        return this.pila.size();
+    }
+
+    // mostrar elemtos de la pila según orden de salida{
+    public void mostrarElementosSegunSalida() {
+
+        for (int i = this.pila.size() - 1; i >= 0; i--) {
+            System.out.print(this.pila.get(i));
+
+        }
+
+    }
+
+    // rellenar pila con array
+    public void rellenar(T[] aux) {
+        for (int i = 0; i < aux.length; i++) {
+            this.pila.add(aux[i]);
+        }
+
+    }
+
+    // sacarElementos dentro de un array
+    public Object[] sacarElementos() {
+
+        Object[] aux = this.pila.toArray();
+        this.pila.clear();
+        return aux;
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Pila{");
+        sb.append("pila=").append(pila);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    //
 }
---
+++ Tema5ejercicios/src/main/java/ejercicio26_pilagenericayejercicio27/PruebaPila.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio26_pilagenericayejercicio27;
+
+/**
+ *
+ * @author nacho
+ */
+public class PruebaPila {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pila<String> aux= new Pila<>();
+        Pila<Cancion> canciones = new Pila<>();
+        
+        String st1="hola";
+        String st2="adios";
+        String st3="pepe";
+        String st4="azul";
+        String st5="perro";
+        String st6="coche";
+        
+        Cancion c1 = new Cancion("hola hola", "Paco", 2.56);
+        Cancion c2 = new Cancion("love", "Beatles", 2.00);
+        Cancion c3 = new Cancion("Balck", "ACDC", 2.45);
+        Cancion c4 = new Cancion("las mareas", "Carmona", 3.00);
+        Cancion c5 = new Cancion("lucky lucky", "Los morancos", 2.56);
+        Cancion c6 = new Cancion("BYe", "antonio flores", 2.56);
+        
+        aux.push(st1);
+        aux.push(st2);
+        aux.push(st3);
+        aux.push(st4);
+        aux.push(st5);
+        aux.push(st6);
+        
+        
+    }
+    
+}
