003fd02b8d8b0992122cb4e2a0a68a8ba45e129f
jsalgua027 <jsalcedoguarde@gmail.com>
2023-02-06 14:42:34 +0100

    Iniciado clase de prueba

---
+++ Tema6Ejercicios/src/main/java/teoria_herencia/Prueba.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teoria_herencia;
+
+/**
+ *
+ * @author nacho
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Turismo t1 = new Turismo(5, true, 45464645L, "DAF454", "Seat", "Ibiza", "Blanco", 12500.2, true);
+        
+       
+
+        
+        
+    }
+    
+}
