4c71bd780c66d778b28d7b20dcd3163ec1544c99
jsalgua027 <jsalcedoguarde@gmail.com>
2023-02-08 14:35:01 +0100

    fibobacci iteractivo y recursivo

--- Tema5ejercicios/src/main/java/teoria_recursividad/Fibobacci.java
+++ Tema5ejercicios/src/main/java/teoria_recursividad/Fibobacci.java
@@ -4,6 +4,8 @@
  */
 package teoria_recursividad;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author nacho
@@ -13,29 +15,48 @@ public class Fibobacci {
     /**
      * @param args the command line arguments
      */
-    
-    
-    public static int fibonacciIteractivo(int numero){
-    int resultado=0;
-    
-    return resultado;
+    public static int fibonacciIteractivo(int numero) {
+       ArrayList<Integer> fibo = new ArrayList<>();
+        for (int i = 0; i <=numero; i++) {
+
+            switch (i) {
+                case 0 -> fibo.add(0);
+                case 1 -> fibo.add(1);
+                default -> fibo.add(fibo.get(i - 1) + fibo.get(i - 2));
+                
+
+            }
+
+        }
+        return fibo.get(numero);
     }
-    
-    
-       public static int fibonacciRecursivo(int numero){
-    int resultado=0;
-    
-           for (int i = 0; i < numero; i++) {
-               
-           }
-    
-    return resultado;
+
+    public static int fibonacciRecursivo(int numero) {
+        
+        if(numero==0){
+        return 0;
+        
+        }
+        if(numero==1){
+         return 1;
+        
+        }else {
+        
+        return (fibonacciRecursivo(numero-1)+fibonacciRecursivo(numero-2));
+        }
+
+        
+      
     }
-    
-    
-    
+
     public static void main(String[] args) {
-        // TODO code application logic here
+         int resultado=fibonacciIteractivo(6);
+         System.out.println("El iteractivo es: "+ resultado);
+         
+         
+         int resultadoFibo= fibonacciRecursivo(6);
+         
+         System.out.println("El recursivo es : "+ resultadoFibo);
     }
-    
+
 }
