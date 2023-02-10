589e8a72433ea5e375dcde4bf42333081a37e6dc
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-02-10 19:08:16 +0100

    Añadidio Main (Prueba) y terminado ejercicio 3

---
+++ Tema6Ejercicios/src/main/java/ejercicio3_persona/PruevaPersona.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio3_persona;
+
+/**
+ *
+ * @author Windows10
+ */
+public class PruevaPersona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Persona p1 = new Persona("Nacho", "12345F", 23);
+      Empleado e1 = new Empleado(1200, "Pepe", "345L", 25);
+      Programador pro1 = new Programador(CategoriaProgamador.JUNIOR, 3500, "Ana", "1111H", 18);
+      // desde la clase programador puedo acceder al metodo subir salario
+      pro1.aumentarSalario(1000);
+      // desde la clase Persona no puedo acceder a subir salario
+      
+    
+        System.out.println(  p1.toString());
+        System.out.println(  e1.toString());
+        System.out.println(  pro1.toString());
+      
+    }
+    
+}
