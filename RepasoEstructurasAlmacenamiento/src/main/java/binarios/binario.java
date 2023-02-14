0c7064d213d1556358db9d4cca1b4515dffd1164
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-02-14 18:13:31 +0100

    terminado ejercicios de matrices

--- RepasoEstructurasAlmacenamiento/src/main/java/binarios/binario.java
+++ RepasoEstructurasAlmacenamiento/src/main/java/binarios/binario.java
@@ -15,8 +15,7 @@ public class binario {
     /**
      * @param args the command line arguments
      */
-    
-     private static Random aleatorio = new Random();
+    private static Random aleatorio = new Random();
 
     public static int aleatorioDentroRango(int min, int max) {
         int numeroGenerado;
@@ -27,42 +26,71 @@ public class binario {
 
     }
 
-    public static int[][] generarMatriz(int filas, int columnas){
-            int[][] aux = new int [filas][columnas];
-            for (int i = 0; i < aux.length; i++) {
-                for (int j = 0; j < aux[i].length; j++) {
-                    aux[i][j]=aleatorioDentroRango(0, 1);
-                }
+    public static int[][] generarMatriz(int filas, int columnas) {
+        int[][] aux = new int[filas][columnas];
+        for (int i = 0; i < aux.length; i++) {
+            for (int j = 0; j < aux[i].length; j++) {
+                aux[i][j] = aleatorioDentroRango(0, 1);
+            }
         }
-            
-            return aux;
+
+        return aux;
     }
-    
-       public static void imprimirMatriz(int[][] aux) {
+
+    public static void imprimirMatriz(int[][] aux) {
 
         for (int i = 0; i < aux.length; i++) {
             for (int j = 0; j < aux[i].length; j++) {
-                System.out.print("|"+aux[i][j]+"|");
+                System.out.print("|" + aux[i][j] + "|");
             }
             System.out.println(" ");
         }
 
     }
+
+   
+
+    public static void comprobarEsquinas(int[][] m) {
+    
+        for (int i = 1; i < m.length - 1; i++) {
+            for (int j = 1; j < m[i].length - 1; j++) {
+                if ((m[i - 1][j - 1] == 1) && (m[i - 1][j + 1] == 1) && (m[i + 1][j - 1] == 1) && (m[i + 1][j + 1] == 1)) {
+
+                    System.out.println("La casilla que reune las caracteristicas es: " + i + "-" + j);
+                }
+            }
+        }
+    }
+
+    public static void busquedaBinaria(int[][] m) {
+
+        for (int i = 1; i < m.length - 1; i++) {
+            for (int j = 1; j < m[i].length - 1; j++) {
+                if (m[i][j] == 0) {
+                    comprobarEsquinas(m);
+                    break;
+                }
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-      int[][] matriz = generarMatriz(5, 5);
+    int[][] matriz = generarMatriz(5, 5);
         imprimirMatriz(matriz);
-        
-         int[][] prueba
+        busquedaBinaria(matriz);
+
+        int[][] prueba
                 = {
                     {0, 1, 0, 1, 0},
-                    {1, 0, 1 ,0, 1},
-                    {0, 1, 0, 1,0},
+                    {1, 0, 0, 0, 1},
+                    {0, 1, 0, 1, 0},
                     {0, 0, 0, 0, 0}
                 };
-        
-        
-        
-        
+        imprimirMatriz(prueba);
+
+        busquedaBinaria(prueba);
+
     }
-    
+
 }
