4e48b8107b6f6b3d980cdc6a6c2f8aaafda5d966
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-02-21 19:11:54 +0100

    .....

--- Ejercicios20Febrero/src/main/java/a/ArrayRecursivo.java
+++ Ejercicios20Febrero/src/main/java/a/ArrayRecursivo.java
@@ -5,13 +5,55 @@
 
 package a;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Windows10
  */
-public class ArrayRecursivo {
 
+  
+
+
+public class ArrayRecursivo {
+  public static int leerNumeroEnteroSinErroresJOptionPane() {
+
+        boolean repetir = true;
+        int numero = 0;  // -1; // Edad no válida
+
+        do {
+            String tmp = JOptionPane.showInputDialog("Introduce el número: ");
+            try {
+                numero = Integer.parseInt(tmp);
+                repetir = false;
+            } catch (NumberFormatException nfe) {
+                JOptionPane.showInputDialog(null, "No has introducido un número entero.");
+            }
+        } while (repetir);
+
+        return numero;
+    }
+  
+//  public static char[] capicuaRecusrivo(char[]aux, int longitud){
+//   
+//      if (longitud==0){
+//      return 
+//      
+//      }
+//  return .
+//  
+ // }
+  
+  
+  
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       
+        int numero= leerNumeroEnteroSinErroresJOptionPane();
+        //trasformo el int en un String
+        String numeroLetra=String.valueOf(numero);
+        //trasformo el string en un array de chars
+        char[] arrayChar= numeroLetra.toCharArray();
+     
+        
     }
 }
